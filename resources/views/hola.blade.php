831260c319bdcc9930013f2aec3836e4eeb8910b
Edwin Castro <141517335+3dwinC4astro@users.noreply.github.com>
2024-10-04 11:30:18 -0500

    Update hola.blade.php

--- resources/views/hola.blade.php
+++ resources/views/hola.blade.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>hola</h1>
+    <table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Roles</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>
+                @foreach($user->roles as $role)
+                    {{ $role->name }}@if(!$loop->last), @endif
+                @endforeach
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
 </body>
 </html>
